c1d8c5d4368cce1e7fda49d9018a3cf2b8bc8ffd
2292527883 <2292527883@qq.com>
2025-05-22 21:33:45 +0800

    1

---
+++ ctldata.py
@@ -0,0 +1,45 @@
+import sqlite3
+import requests
+import json
+
+
+def create_tables():
+    
+    conn = sqlite3.connect('./Datas/iteamdata.db')
+    cursor = conn.cursor()
+
+    # 主表：存储类型基本信息
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS types (
+            type_id INTEGER PRIMARY KEY,
+            en_name TEXT,
+            zh_name TEXT,
+            group_id INTEGER,
+            group_name TEXT,
+            market_group_id INTEGER,
+            market_group_name TEXT,
+            mass REAL,
+            volume REAL,
+            published BOOLEAN
+        )
+        ''')
+def getidapi():
+    idapi='https://sde.jita.space/universe/types'
+    response=requests.get(idapi)
+    if response.status_code != 200:
+        
+        
+
+    cursor.execute("""
+        
+
+
+""")
+
+ 
+
+    conn.commit()
+    conn.close()
+    
+if __name__ == "__main__":
+    create_tables()
---
+++ serch.py
@@ -0,0 +1,10 @@
+#接受用户输入的字符串，通过字符串查询数据库对应的物品id，并返回物品名称、最高价格、最低价格和30天平均交易量；工业所需的物品，以及价格和数量。
+import json
+import pymssql
+
+def serch():
+    # 连接到数据库
+    aaa=None
+    print(aaa)
+    return 0
+
